89610f0ade2f03f6c933c59274c6d0264665df0d
sdo3 <do.ngoc.son2@usol-v.com.vn>
2019-04-24 17:42:42 +0700

    Two

--- app/src/main/java/com/example/startup/MainActivity.java
+++ app/src/main/java/com/example/startup/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.startup;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout
+
+    }
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
